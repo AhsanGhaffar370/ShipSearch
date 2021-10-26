d354f00a77fe32390af5ba936660083e9780762f
Muhammad Faraz <muhammadfaraz991@gmail.com>
2021-10-26 23:15:07 +0500

    Change send button icon to button and change some issues

--- resources/views/vendor/Chatify/layouts/headLinks.blade.php
+++ resources/views/vendor/Chatify/layouts/headLinks.blade.php
@@ -4,28 +4,31 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="route" content="{{ $route }}">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<meta name="url" content="{{ url('').'/'.config('chatify.routes.prefix') }}" data-user="{{ session('front_uid') }}">
+<meta name="url" content="{{ url('') . '/' . config('chatify.routes.prefix') }}"
+    data-user="{{ session('front_uid') }}">
 
 {{-- scripts --}}
-<script
-  src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="{{ asset('js/chatify/font.awesome.min.js') }}"></script>
 <script src="{{ asset('js/chatify/autosize.js') }}"></script>
 <script src="{{ asset('js/app.js') }}"></script>
 <script src='https://unpkg.com/nprogress@0.2.0/nprogress.js'></script>
 
 {{-- styles --}}
-<link rel='stylesheet' href='https://unpkg.com/nprogress@0.2.0/nprogress.css'/>
+<link rel='stylesheet' href='https://unpkg.com/nprogress@0.2.0/nprogress.css' />
 <link href="{{ asset('css/chatify/style.css') }}" rel="stylesheet" />
-<link href="{{ asset('css/chatify/'.$dark_mode.'.mode.css') }}" rel="stylesheet" />
+<link href="{{ asset('css/chatify/' . $dark_mode . '.mode.css') }}" rel="stylesheet" />
 <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
-
+<link
+    href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+    rel="stylesheet">
 
 <style>
-  p, span{
-    font-family: 'Eras Medium ITC', sans-serif !important;
-  }
-</style>
-{{-- Messenger Color Style--}}
-@include('Chatify::layouts.messengerColor')
+    p,
+    span {
+        font-family: 'Lato', sans-serif !important;
+    }
 
+</style>
+{{-- Messenger Color Style --}}
+@include('Chatify::layouts.messengerColor')
--- resources/views/vendor/Chatify/layouts/sendForm.blade.php
+++ resources/views/vendor/Chatify/layouts/sendForm.blade.php
@@ -1,13 +1,17 @@
+<style>
+    .button5 {border-radius: 35%;font-size: 13px;color: white;background-color:#4d998d;}
+    .button5:hover{color: white}
+</style>
 <div class="messenger-sendCard">
     <form id="message-form" method="POST" action="{{ route('send.message') }}" enctype="multipart/form-data">
         @csrf
-        <label><span class="fas fa-paperclip"></span><input disabled='disabled' type="file" class="upload-attachment" name="file" accept="image/*, .txt, .rar, .zip" /></label>
+        {{-- <label><span class="fas fa-paperclip"></span><input disabled='disabled' type="file" class="upload-attachment" name="file" accept="image/*, .txt, .rar, .zip" /></label> --}}
         <textarea readonly='readonly' name="message" class="m-send app-scroll" placeholder="Type a message.."></textarea>
         <button disabled='disabled'>
             
             {{-- <span class="fas fa-paper-plane"></span> --}}
             {{-- <i class="fad fa-paper-plane"></i> --}}
-            <span class="fas fa-location-arrow"></span>
+            <span class="btn button5 p-2 mx-3">Send</span>
         </button>
     </form>
 </div>
